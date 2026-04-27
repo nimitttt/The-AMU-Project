b7a2fa17d4e900ebbc31cb19aacb9e276a352b37
nimitttt <nimitchauhan0425@gmail.com>
2026-04-27 13:55:43 +0530

    Implement hamburger menu toggle functionality
    Added functionality for hamburger menu toggle and event listeners.

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// HAMBURGER ICON
+
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+const overlay = document.getElementById("overlay");
+const menuLinks = document.querySelectorAll(".menu a");
+
+function toggleMenu() {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+overlay.addEventListener("click", toggleMenu);
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", toggleMenu);
+});
+
+// HERO SEC SLIDER
+
 const track = document.querySelector(".track");
 const slides = document.querySelectorAll(".slide-holder");
 const dots = document.querySelectorAll(".dot");
